78fd612f410b1d7a42cb98361950a38beb4d0706
iotassss <shokutaa@gmail.com>
2023-06-13 08:58:12 +0900

    ユーザー照会機能

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,35 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import axios from 'axios';
 
 export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    })
+    }),
   ],
+  callbacks: {
+    async jwt({ token, user, account}) {
+      // providerとproviderAccountIdを使ってユーザーを検索、なければ作成
+      if (account) {
+        await axios.post('http://localhost/api/user', {
+          provider: account.provider,
+          providerAccountId: account.providerAccountId,
+        }).then((response) => {
+          token.sub = response.data.uuid;
+        }).catch((error) => {
+          console.log('Error in axios post', error);
+        });
+      }
+
+      return token;
+    },
+    async session({ session, user, token }) {
+      session.sub = token.sub;
+      return session;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
