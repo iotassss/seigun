f414aa78cd933258c29ee2afd5d4744d149c234c
iotassss <shokutaa@gmail.com>
2023-06-16 01:42:54 +0900

    feat: AuthorizationヘッダーにAPIキーを設定してリクエスト

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,13 +13,21 @@ export const authOptions = {
     async jwt({ token, user, account}) {
       // providerとproviderAccountIdを使ってユーザーを検索、なければ作成
       if (account) {
-        await axios.post('http://localhost/api/user', {
-          provider: account.provider,
-          providerAccountId: account.providerAccountId,
-        }).then((response) => {
+        await axios.post('http://localhost/api/user',
+          {
+            provider: account.provider,
+            providerAccountId: account.providerAccountId,
+          },
+          {
+            headers: {
+              Authorization: process.env.LARAVEL_API_KEY,
+            },
+          },
+        ).then((response) => {
           token.sub = response.data.uuid;
         }).catch((error) => {
           console.log('Error in axios post', error);
+          throw new Error('Unable to find or create user');
         });
       }
 
