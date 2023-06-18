ef6eccc2eff2d458d6dd41cf5f9a12df743c3993
iotassss <shokutaa@gmail.com>
2023-06-19 02:13:57 +0900

    feat: 勧誘ボタン設置

--- components/Recruit/index.js
+++ components/Recruit/index.js
@@ -7,6 +7,17 @@ const Recruit = () => {
         <source media="(max-width: 767px)" srcSet="/images/recruit01_sp.jpg" />
         <img src="/images/recruit01.jpg" alt="recruit01" className={styles.recruitImg} />
       </picture>
+      <div className={styles.recruitPanel}>
+        <h1 className={styles.title}>
+          西軍へようこそ
+        </h1>
+        <p>
+          豊臣家を支える唯一の正規軍西軍は、あなたの力を必要としています。
+        </p>
+        <button className={styles.recruitBtn}>
+          ログインして参戦する
+        </button>
+      </div>
     </div>
   );
 };
