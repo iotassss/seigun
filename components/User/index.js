d071aebd72e1f89249eecf03bc9cf7ca00a2631c
iotassss <shokutaa@gmail.com>
2023-06-18 16:02:20 +0900

    feat: Userコンポーネントでセッション情報読み込み

--- components/User/index.js
+++ components/User/index.js
@@ -1,7 +1,14 @@
+import { useSession } from "next-auth/react";
+
 const User = () => {
+  const { data: session } = useSession();
+
   return (
     <div>
       <h1>User</h1>
+      <p>
+        {session ? `${session.user.name}さんこんにちは` : "こんにちは"}
+      </p>
     </div>
   )
 };
