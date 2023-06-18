5f88aaf076f7fbbbdfa172ce17a9e9dbb285f8be
iotassss <shokutaa@gmail.com>
2023-06-18 15:46:49 +0900

    add: userコンポーネント

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -2,34 +2,29 @@ import Link from 'next/link';
 import styles from './styles.module.css';
 import NavAvatar from '../NavAvatar/index.js'
 
-const Navigation = () => (
-  <div className={styles.navigation}>
-    <h1 className={styles.title}>
-      <Link href="/">
-        Next.js
-      </Link>
-    </h1>
-    <nav>
-      <ul className={styles.navlist}>
-        <li>
-          <Link href="/">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact">
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </nav>
-    <NavAvatar />
-  </div>
-);
+const Navigation = ({setPanel}) => {
+
+
+  return (
+    <div className={styles.navigation}>
+      <h1 className={styles.title}>
+        <Link href="/">
+          Next.js
+        </Link>
+      </h1>
+      <nav>
+        <ul className={styles.navlist}>
+          <li>
+            <button onClick={() => setPanel(0)}>Home</button>
+          </li>
+          <li>
+            <button onClick={() => setPanel(1)}>User</button>
+          </li>
+        </ul>
+      </nav>
+      <NavAvatar />
+    </div>
+  );
+};
 
 export default Navigation
