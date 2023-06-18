4924e6421aff3602892f569e7b81c2b76b933390
iotassss <shokutaa@gmail.com>
2023-06-19 01:39:25 +0900

    add: ImportantNoticeコンポーネント

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,11 +1,13 @@
 import styles from './styles.module.css';
 import Recruit from '../Recruit';
+import ImportantNotice from '../ImportantNotice';
 import News from '../News';
 
 const Home = () => {
   return (
     <main className={styles.main}>
       <Recruit />
+      <ImportantNotice />
       <News />
     </main>
   )
---
+++ components/ImportantNotice/index.js
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+
+const ImportantNotice = () => {
+  return (
+    <h2>
+      重要なお知らせ
+    </h2>
+  );
+}
+
+export default ImportantNotice;
