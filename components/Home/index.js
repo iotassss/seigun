4f9ba8d224e8c855d79b741c94940aae45e37ce3
iotassss <shokutaa@gmail.com>
2023-06-18 17:44:57 +0900

    add: Newsコンポーネント

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,44 +1,12 @@
-import Link from 'next/link';
 import styles from './styles.module.css';
 import Recruit from '../Recruit';
+import News from '../News';
 
 const Home = () => {
   return (
     <main className={styles.main}>
       <Recruit />
-
-      <div className={styles.grid}>
-
-        <Link href="https://nextjs.org/docs" className={styles.card}>
-          <h2>Documentation &rarr;</h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </Link>
-
-        <Link href="https://nextjs.org/learn" className={styles.card}>
-          <h2>Learn &rarr;</h2>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </Link>
-
-        <Link
-          href="https://github.com/vercel/next.js/tree/canary/examples"
-          className={styles.card}
-        >
-          <h2>Examples &rarr;</h2>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </Link>
-
-        <Link
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.card}
-        >
-          <h2>Deploy &rarr;</h2>
-          <p>
-            Instantly deploy your Next.js site to a public URL with Vercel.
-          </p>
-        </Link>
-      </div>
+      <News />
     </main>
   )
 }
