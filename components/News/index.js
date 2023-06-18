6ae8296fd28b3117e7e503e308eea89ce454de8c
iotassss <shokutaa@gmail.com>
2023-06-18 21:34:25 +0900

    feat: Newsコンポーネントのモック作成

--- components/News/index.js
+++ components/News/index.js
@@ -4,39 +4,51 @@ import styles from "./styles.module.css";
 const News = () => {
   return (
     <>
-      <h2 className={styles.title}>News</h2>
-      <div className={styles.grid}>
-
-        <Link href="https://nextjs.org/docs" className={styles.card}>
-          <h2>Documentation &rarr;</h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </Link>
-
-        <Link href="https://nextjs.org/learn" className={styles.card}>
-          <h2>Learn &rarr;</h2>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </Link>
-
-        <Link
-          href="https://github.com/vercel/next.js/tree/canary/examples"
-          className={styles.card}
-        >
-          <h2>Examples &rarr;</h2>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </Link>
-
-        <Link
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.card}
-        >
-          <h2>Deploy &rarr;</h2>
-          <p>
-            Instantly deploy your Next.js site to a public URL with Vercel.
-          </p>
-        </Link>
-      </div>
+      <h2 className={styles.title}>
+        News
+      </h2>
+      <article className={styles.article}>
+        <section className={styles.section}>
+          <picture>
+            <img src="/images/sekigahara.jpg" alt="sekigahara" className={styles.img} />
+          </picture>
+          <div className={styles.text}>
+            <h3>
+              関ヶ原の戦いの解説
+            </h3>
+          </div>
+        </section>
+        <section className={styles.section}>
+          <picture>
+            <img src="/images/sekigahara.jpg" alt="sekigahara" className={styles.img} />
+          </picture>
+          <div className={styles.text}>
+            <h3>
+              関ヶ原の戦いの解説
+            </h3>
+          </div>
+        </section>
+        <section className={styles.section}>
+          <picture>
+            <img src="/images/sekigahara.jpg" alt="sekigahara" className={styles.img} />
+          </picture>
+          <div className={styles.text}>
+            <h3>
+              関ヶ原の戦いの解説
+            </h3>
+          </div>
+        </section>
+        <section className={styles.section}>
+          <picture>
+            <img src="/images/sekigahara.jpg" alt="sekigahara" className={styles.img} />
+          </picture>
+          <div className={styles.text}>
+            <h3>
+              関ヶ原の戦いの解説
+            </h3>
+          </div>
+        </section>
+      </article>
     </>
   );
 };
