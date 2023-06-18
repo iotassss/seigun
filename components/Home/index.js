0eb6d3ad6527d6c56673c2ef527a5d5b74062da4
iotassss <shokutaa@gmail.com>
2023-06-18 17:49:23 +0900

    add: MessageBoardコンポーネント

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,11 +1,13 @@
 import styles from './styles.module.css';
 import Recruit from '../Recruit';
+import MessageBoard from '../MessageBoard';
 import News from '../News';
 
 const Home = () => {
   return (
     <main className={styles.main}>
       <Recruit />
+      <MessageBoard />
       <News />
     </main>
   )
---
+++ components/MessageBoard/index.js
@@ -0,0 +1,9 @@
+const MessageBoard = () => {
+  return (
+    <div>
+      <h2>Message Board</h2>
+    </div>
+  );
+};
+
+export default MessageBoard;
