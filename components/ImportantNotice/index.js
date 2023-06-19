e6a691e0a441b81945a4e04124bfa06c64fdca76
iotassss <shokutaa@gmail.com>
2023-06-19 19:02:12 +0900

    feat: ImportantNoticeコンポーネントモック

--- components/ImportantNotice/index.js
+++ components/ImportantNotice/index.js
@@ -2,9 +2,17 @@ import styles from './styles.module.css';
 
 const ImportantNotice = () => {
   return (
-    <h2>
-      重要なお知らせ
-    </h2>
+    <div className={styles.background}>
+      <h2 className={styles.title}>
+        重要なお知らせ
+      </h2>
+      <h3>
+        挙兵について
+      </h3>
+      <p>
+        謀反の疑いがある徳川家康に対し、挙兵を行いました。
+      </p>
+    </div>
   );
 }
 
