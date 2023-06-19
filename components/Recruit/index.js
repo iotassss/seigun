7cbda93403b76d35e52117fe246d4cd4c0c6dcc2
iotassss <shokutaa@gmail.com>
2023-06-19 21:55:19 +0900

    feat: Home画面軽微な修正

--- components/Recruit/index.js
+++ components/Recruit/index.js
@@ -4,7 +4,7 @@ const Recruit = () => {
   return (
     <div>
       <picture>
-        <source media="(max-width: 767px)" srcSet="/images/recruit01_sp.jpg" />
+        <source media="(max-width: 480px)" srcSet="/images/recruit01_sp.jpg" />
         <img src="/images/recruit01.jpg" alt="recruit01" className={styles.recruitImg} />
       </picture>
       <div className={styles.recruitPanel}>
