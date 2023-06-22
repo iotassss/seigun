905def219df54a1f9226272e8307a08a541d61a2
iotassss <shokutaa@gmail.com>
2023-06-22 10:13:32 +0900

    style: Recruitコンポーネント見た目調整

--- components/Recruit/index.js
+++ components/Recruit/index.js
@@ -1,22 +1,25 @@
 import styles from './styles.module.css';
+import { FaSignInAlt } from 'react-icons/fa';
 
 const Recruit = () => {
   return (
-    <div>
+    <div className={styles.recruit}>
       <picture>
         <source media="(max-width: 480px)" srcSet="/images/recruit01_sp.jpg" />
         <img src="/images/recruit01.jpg" alt="recruit01" className={styles.recruitImg} />
       </picture>
-      <div className={styles.recruitPanel}>
-        <h1 className={styles.title}>
-          西軍へようこそ
-        </h1>
-        <p>
-          豊臣家を支える唯一の正規軍西軍は、あなたの力を必要としています。
-        </p>
-        <button className={styles.recruitBtn}>
-          ログインして参戦する
-        </button>
+      <div className={styles.recruitPanelcontainer}>
+        <div className={styles.recruitPanel}>
+          <p>
+            西軍は豊臣家を支える<span>唯一の正規軍</span>です。あなたも西軍に参加して、豊臣家を支えませんか？
+          </p>
+          <button className={styles.recruitBtn}>
+            ログインして参戦
+            <span className={styles.signInIcon}>
+              <FaSignInAlt />
+            </span>
+          </button>
+        </div>
       </div>
     </div>
   );
