2dc02d7b3aeadf565a059329a4513321065580f7
iotassss <shokutaa@gmail.com>
2023-06-22 19:10:47 +0900

    「重要なお知らせ」見た目調整

--- components/ImportantNotice/index.js
+++ components/ImportantNotice/index.js
@@ -3,15 +3,20 @@ import styles from './styles.module.css';
 const ImportantNotice = () => {
   return (
     <div className={styles.background}>
-      <h2 className={styles.title}>
-        重要なお知らせ
-      </h2>
-      <h3>
-        挙兵について
-      </h3>
-      <p>
-        謀反の疑いがある徳川家康に対し、挙兵を行いました。
-      </p>
+      <div className={styles.container}>
+        <h2 className={styles.title}>
+          重要なお知らせ
+        </h2>
+        <div className={styles.notice}>
+          <div className={styles.date}>1600年7月19日</div>
+          <h3>
+            挙兵について
+          </h3>
+          <p>
+            太閤様の御遺命に背き謀反を企てているとして、徳川家康に対して討伐命令が下されました。これを受けて毛利輝元様を総大将として家康討伐軍(西軍)を編成いたしました。詳しくはこちらをご覧ください。
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
